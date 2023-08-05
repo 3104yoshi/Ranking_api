6361af675c2255dba4920eeb5cc466dc1137715c
3104yoshi <3104adgjmp@gmail.com>
2023-08-06 00:19:33 +0900

    add setting proxy

---
+++ frontend/vue.config.js
@@ -0,0 +1,9 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  outputDir: '../dist/',
+  assetsDir: 'static',
+  devServer: {
+    proxy: 'http://localhost:5000'
+  },
+  transpileDependencies: true
+})
